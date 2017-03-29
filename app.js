6163cd9aebd34684f5caca70cf6445a0deaa4aaf
diego <diegowebd@hotmail.com>
2017-03-29 14:34:45 -0300

    Initial add of Jwt and Jwt Express

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var session = require('express-session');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
+var jwtSecret = 'f12lf1lk2flk1lj2kf';
 
 var index = require('./routes/index');
 var eventos = require('./routes/eventos');
@@ -10,6 +13,16 @@ var users = require('./routes/users');
 var timeline = require('./routes/timeline');
 
 var app = express();
+
+/**
+ * Jwt Express
+ */
+// app.use(
+//     expressJwt(
+//         { secret: jwtSecret })
+//         .unless(
+//         { path: ['/api/eventos', '/api/users/login' ] })
+// );
 
 /**
  * Starting Cors Middleware
@@ -25,7 +38,7 @@ app.use(logger);
 /**
  * Defining Sessions
  */
-app.use(session({ 
+app.use(session({
     secret: 'some-secret',
     saveUninitialized: false,
     resave: true
